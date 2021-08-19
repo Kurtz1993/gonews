03a8529007f986d00f8560ec599c34e43cac8581
Luis Hernandez <luis.hlopez@outlook.com>
2021-08-18 19:28:01 -0500

    Read and iterate over sitemap

--- main.go
+++ main.go
@@ -13,32 +13,41 @@ type SitemapIndex struct {
 }
 
 type News struct {
-	Titles []string `xml:"url>news>title"`
-	Keywords []string `xml:"url>news>keywords"`
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
 	Locations []string `xml:"url>loc"`
 }
 
-func main() {
-	var s SitemapIndex
-	var n News
+type NewsMap struct {
+	Keyword  string
+	Location string
+}
 
+func main() {
+	var n News
+	newsMap := make(map[string]NewsMap)
+
+	// location represents the value
+	// for _, location := range s.Locations {
 	response, _ := http.Get("https://www.nytimes.com/sitemaps/new/news.xml.gz")
 	bytes, _ := io.ReadAll(response.Body)
 	response.Body.Close()
 
-	// Parses the bytes from the XML response to the specified type
-	// of the s variable
-	xml.Unmarshal(bytes, &s)
+	xml.Unmarshal(bytes, &n)
 
-	// This works similar to a for-of loop. The "_" will be the index
-	// location represents the value
-	for _, location := range s.Locations {
-		response, _ := http.Get(location)
-		bytes, _ := io.ReadAll(response.Body)
-		response.Body.Close()
+	// This works similar to a for-of loop. The "_" will be the value.
+	// It can also be simplified by removing the "_"
+	for idx, _ := range n.Keywords {
+		newsMap[n.Titles[idx]] = NewsMap{
+			Keyword:  n.Keywords[idx],
+			Location: n.Locations[idx],
+		}
+	}
+	// }
 
-		xml.Unmarshal(bytes, &n)
-
-		fmt.Println(n.Locations)
+	for idx, data := range newsMap {
+		fmt.Println("\n\n\n", "Title:", idx)
+		fmt.Println("\n", "Keyword:", data.Keyword)
+		fmt.Println("\n", "Location:", data.Location)
 	}
 }
